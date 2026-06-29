bd10ba3259e84dc761047f6982d07aad834f5c19
Chars Volta <charsvolta@gmail.com>
2026-06-29 00:56:55 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,17 +1,55 @@
 import { iniciarProductos } from "./productos.js";
 
-export async function cargarPagina(nombre){
+export async function cargarPagina(nombre) {
 
-const contenido=document.getElementById("contenido");
+    const contenido = document.getElementById("contenido");
+    try {
 
-const pagina=await fetch(`paginas/${nombre}.html`);
+        const respuesta = await fetch(`paginas/${nombre}.html`);
+        if (!respuesta.ok) {
+            throw new Error("No se pudo cargar la página");
+        }
 
-contenido.innerHTML=await pagina.text();
+        const html = await respuesta.text();
+        contenido.innerHTML = html;
+        switch (nombre) {
+            case "productos":
+                iniciarProductos();
+                break;
+            case "dashboard":
+                break;
 
-if(nombre==="productos"){
+            case "stock":
+                break;
 
-iniciarProductos();
+            case "ventas":
+                break;
 
-}
+            case "compras":
+                break;
 
+            case "clientes":
+                break;
+
+            case "empleados":
+                break;
+
+            case "estadisticas":
+                break;
+
+            case "configuracion":
+                break;
+
+        }
+    }
+
+    catch (error) {
+        console.error(error);
+        contenido.innerHTML = `
+        <div class="tarjeta">
+            <h2>Error</h2>
+            <p>No se pudo cargar la página.</p>
+        </div>
+        `;
+    }
 }
