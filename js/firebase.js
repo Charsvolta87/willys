54b7e6c002c496fc2f7c982f5aa192928125ed9b
Chars Volta <charsvolta@gmail.com>
2026-06-28 22:46:41 -0300

    Upgrade Firebase SDK and reconfigure settings
    Updated Firebase SDK version and configuration details.

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,30 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+/* ==========================================
+   WILLY'S ADMIN
+   FIREBASE
+========================================== */
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import {
-  getDatabase,
-  ref,
-  push,
-  set,
-  get,
-  child,
-  update,
-  remove
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+    getDatabase, ref, set, get, push, update, remove, onValue} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
+
+// CONFIGURACIÓN
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGIkqTCq8tXbclZVNUnEmPUgNgVRXz_H0",
-  authDomain: "datafilms-d3160.firebaseapp.com",
-  databaseURL: "https://datafilms-d3160-default-rtdb.firebaseio.com",
-  projectId: "datafilms-d3160",
-  storageBucket: "datafilms-d3160.appspot.com",
-  messagingSenderId: "554321965868",
-  appId: "1:554321965868:web:2420b38a02e9e8b981b289"
+    apiKey: "AIzaSyAz9XHfFQWgTClf8gLn4Ri12JHVitAmA4U",
+    authDomain: "willy-s-6f6e4.firebaseapp.com",
+    databaseURL: "https://willy-s-6f6e4-default-rtdb.firebaseio.com",
+    projectId: "willy-s-6f6e4",
+    storageBucket: "willy-s-6f6e4.firebasestorage.app",
+    messagingSenderId: "852499850297",
+    appId: "1:852499850297:web:1924f97dd688bbf4ac19e8",
+    measurementId: "G-1HYXT4Z754"
 };
 
-const app = initializeApp(firebaseConfig);
+// Inicializar Firebase
 
-// Realtime Database
-export const db = getDatabase(app);
-export { ref, push, set, get, child, update, remove };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Exportamos todo para el resto del sistema
+
+export {db, ref, set, get, push, update, remove, onValue};
