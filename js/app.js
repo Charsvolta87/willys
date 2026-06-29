cfb2a2937f47bfb2dd24d3a639104a4092820fd3
Chars Volta <charsvolta@gmail.com>
2026-06-28 23:13:04 -0300

    Implement menu button click events
    Added event listeners to menu buttons for page navigation.

--- js/app.js
+++ js/app.js
@@ -23,3 +23,20 @@ get(ref(db, "/"))
     console.error(error);
 });
 cargarPagina("dashboard");
+const botones =
+document.querySelectorAll(".menu");
+
+/*---------------------*/
+/********BOTONES********/
+/*---------------------*/
+botones.forEach(boton=>{
+    boton.addEventListener("click",()=>{
+        botones.forEach(b=>b.classList.remove("activo"));
+        boton.classList.add("activo");
+        const pagina =
+        boton.dataset.pagina;
+        document.querySelector("header h1").textContent =
+        boton.textContent.trim();
+        cargarPagina(pagina);
+    });
+});
